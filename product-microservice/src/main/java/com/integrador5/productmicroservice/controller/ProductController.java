d6527b6ab6c91d89924fbbd78f39691f4653623e
Benjamin <aldayabenjamin@gmail.com>
2022-11-09 21:40:49 -0300

    Product Controller

--- product-microservice/src/main/java/com/integrador5/productmicroservice/controller/ProductController.java
+++ product-microservice/src/main/java/com/integrador5/productmicroservice/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.integrador5.productmicroservice.controller;
 import com.integrador5.productmicroservice.models.Product;
 import com.integrador5.productmicroservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,21 @@ public class ProductController {
     @GetMapping(value = "/")
     public List<Product> getAllProducts(){
         return this.productService.getAllProducts();
+    }
+
+    @PostMapping(value = "/insert")
+    public void insertarProducto(@RequestBody Product product){
+        return this.productService.insertProduct(product);
+    }
+
+
+    @DeleteMapping(value = "/delete/id/{id}")
+    public void deleteProduct(@PathVariable Integer id){
+        this.productService.deleteProduct(id);
+    }
+
+    @PutMapping(value="/update/id/{id}")
+    public void updateProduct(@PathVariable Integer id, @RequestBody Product product){
+        this.productService.updateProduct(id,product);
     }
 }

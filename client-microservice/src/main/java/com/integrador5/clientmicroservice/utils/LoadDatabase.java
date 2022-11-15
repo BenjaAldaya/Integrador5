87329eb085ff429c47103cc797a4e7030d953c66
Benjamin <aldayabenjamin@gmail.com>
2022-11-15 01:57:44 -0300

    CLient microservice

---
+++ client-microservice/src/main/java/com/integrador5/clientmicroservice/utils/LoadDatabase.java
@@ -0,0 +1,28 @@
+package com.integrador5.clientmicroservice.utils;
+
+import com.integrador5.clientmicroservice.controller.ClientController;
+import com.integrador5.clientmicroservice.model.Client;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(ClientController ctrlCarrera) {
+        return args -> {
+            Client c1 = new Client("Aldaya Benjamin");
+            Client c2 = new Client("Binetti Facundo");
+            Client c3 = new Client("Fernandez Manuela");
+            Client c4 = new Client("Carrera Tomas");
+            Client c5 = new Client("Rust Matias");
+            ctrlCarrera.insertClient(c1);
+            ctrlCarrera.insertClient(c2);
+            ctrlCarrera.insertClient(c3);
+            ctrlCarrera.insertClient(c4);
+            ctrlCarrera.insertClient(c5);
+            System.out.println("Clientes cargados");
+        };
+    }
+}

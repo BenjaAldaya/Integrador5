a0ddc1d4b118458787bb439fd281362500169a91
Manuela Fernandez <manu.fer.0396@gmail.com>
2022-11-18 02:25:43 -0300

    Agregado de capa de seguridad JWT a endpoints de product-microservice

--- product-microservice/src/main/java/com/integrador5/productmicroservice/ProductMicroserviceApplication.java
+++ product-microservice/src/main/java/com/integrador5/productmicroservice/ProductMicroserviceApplication.java
@@ -1,13 +1,37 @@
 package com.integrador5.productmicroservice;
 
+import com.integrador5.productmicroservice.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class ProductMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductMicroserviceApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			//desactiv el metodo por defecto
+			http.csrf().disable()
+					//agrega el metodo de filtrado q codificamos nosotros
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/user").permitAll()
+					.antMatchers("/api/products/**").authenticated()
+					.anyRequest().permitAll();
+		}
 	}
 
 }
---
+++ product-microservice/src/main/java/com/integrador5/productmicroservice/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.integrador5.productmicroservice.controller;
+
+import com.integrador5.productmicroservice.DTO.UserDTO;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@Tag(name="User login", description="User login related resources")
+public class UserController {
+    @PostMapping("user")
+    public UserDTO login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
+
+        String token = getJWTToken(username);
+        UserDTO user = new UserDTO();
+        user.setUser(username);
+        user.setToken(token);
+        return user;
+
+    }
+
+    private String getJWTToken(String username) {
+        String secretKey = "grupo1-secret-key-product-ms";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token = Jwts
+                .builder()
+                .setId("softtekJWT")
+                .setSubject(username)
+                .claim("authorities",
+                        grantedAuthorities.stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS512,
+                        secretKey.getBytes()).compact();
+
+        return "Bearer " + token;
+    }
+}
